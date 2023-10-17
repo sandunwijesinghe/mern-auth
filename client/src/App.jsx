5a42704da8b84aaf0faa5c3f79d81b1109dbd282
Sandun Wijesinghe <sandunnw@gmail.com>
2023-10-17 23:10:00 +0530

    add routes and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,25 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
 
 function App() {
-
-  return (
-    <>
-<div>
-
-
-
-
-</div>
-    </>
-  )
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/sign-in" element={<SignIn />} />
+                    <Route path="/sign-up" element={<SignUp />} />
+                    <Route path="/profile" element={<Profile />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
-export default App
+export default App;
